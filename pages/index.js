5441cd1346772c0f62d78ba73a26ae216f5a7716
Davi15S <nguyenxd072@gmail.com>
2022-03-08 10:43:38 +0100

    navbar animations

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,13 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Navbar from '../components/Navbar'
 
 export default function Home() {
   return (
     <div>
 
       {/* Navbar */}
-      
+      <Navbar />
     </div>
   )
 }
