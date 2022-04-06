44e4d4d2bb1d2ac9e797b09418783fb249479eb0
Davi15S <nguyenxd072@gmail.com>
2022-04-06 22:48:25 +0200

    init

--- components/Body/Comment.js
+++ components/Body/Comment.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Comment() {
   return (
     <div className='flex justify-center text-center bg-[#1c1c1c] text-white p-4 font-semibold'>
-        This site was create just for education purposes. Visit <span className='px-1 underline'> <a href="https://qarea.com/" target="_blank">qarea.com</a> </span> for the original site.
+        This site was created just for education purposes. Visit <span className='px-1 underline'> <a href="https://qarea.com/" target="_blank">qarea.com</a> </span> for the original site.
     </div>
   )
 }
